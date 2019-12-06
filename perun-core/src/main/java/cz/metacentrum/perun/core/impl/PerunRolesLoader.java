b72c91d60627d2adee8f4fdfa11dbfd5d03d9848
Peter Balcirak <peter.balcirak@gmail.com>
2019-12-06 08:21:43 +0100

    added constants to PerunRolesLoader

--- perun-core/src/main/java/cz/metacentrum/perun/core/impl/PerunRolesLoader.java
+++ perun-core/src/main/java/cz/metacentrum/perun/core/impl/PerunRolesLoader.java
@@ -35,11 +35,27 @@ public class PerunRolesLoader {
 
 	private Resource configurationPath;
 
+	//Constants
+	private static final String PERUN_ROLES = "perun_roles";
+	private static final String ROLES_ID_SEQ = "roles_id_seq";
+	private static final String PERUN_POLICIES = "perun_policies";
+	private static final String POLICY_ROLES = "policy_roles";
+	private static final String INCLUDE_POLICIES = "include_policies";
+	private static final String PERUN_ACTION_TYPES = "perun_action_types";
+	private static final String NAME = "name";
+	private static final String OBJECT = "object";
+	private static final String DESCRIPTION = "description";
+	private static final String ACTION_TYPES_SEQ = "action_types_seq";
+	private static final String PERUN_ROLES_MANAGEMENT = "perun_roles_management";
+	private static final String PRIVILEGED_ROLES = "privileged_roles";
+	private static final String ENTITIES_TO_MANAGE = "entities_to_manage";
+	private static final String ASSIGN_TO_OBJECTS = "assign_to_objects";
+
 	public void loadPerunRoles(JdbcPerunTemplate jdbc) {
 
 		JsonNode rootNode = loadConfigurationFile();
 
-		JsonNode rolesNode = rootNode.get("perun_roles");
+		JsonNode rolesNode = rootNode.get(PERUN_ROLES);
 		List<String> roles = objectMapper.convertValue(rolesNode, new TypeReference<List<String>>() {});
 
 		// Check if all roles defined in class Role exists in the DB
@@ -50,7 +66,7 @@ public class PerunRolesLoader {
 					if (BeansUtils.isPerunReadOnly()) {
 						throw new InternalErrorException("One of default roles not exists in DB - " + role);
 					} else {
-						int newId = Utils.getNewId(jdbc, "roles_id_seq");
+						int newId = Utils.getNewId(jdbc, ROLES_ID_SEQ);
 						jdbc.update("insert into roles (id, name) values (?,?)", newId, role.toLowerCase());
 					}
 				}
@@ -69,7 +85,7 @@ public class PerunRolesLoader {
 		List<PerunPolicy> policies = new ArrayList<>();
 		JsonNode rootNode = loadConfigurationFile();
 		//Fetch all policies from the configuration file
-		JsonNode policiesNode = rootNode.get("perun_policies");
+		JsonNode policiesNode = rootNode.get(PERUN_POLICIES);
 
 		// For each policy node construct PerunPolicy and add it to the list
 		Iterator<String> policyNames = policiesNode.fieldNames();
@@ -77,7 +93,7 @@ public class PerunRolesLoader {
 			String policyName = policyNames.next();
 			JsonNode policyNode = policiesNode.get(policyName);
 			List<Map<String, String>> perunRoles = new ArrayList<>();
-			JsonNode perunRolesNode = policyNode.get("policy_roles");
+			JsonNode perunRolesNode = policyNode.get(POLICY_ROLES);
 
 			//Field policy_roles is saved as List of maps in the for loop
 			for (JsonNode perunRoleNode : perunRolesNode) {
@@ -86,7 +102,7 @@ public class PerunRolesLoader {
 			}
 
 			//Field include_policies is saved as List of Strings.
-			List<String> includePolicies = new ArrayList<>(objectMapper.convertValue(policyNode.get("include_policies"), new TypeReference<List<String>>() {}));
+			List<String> includePolicies = new ArrayList<>(objectMapper.convertValue(policyNode.get(INCLUDE_POLICIES), new TypeReference<List<String>>() {}));
 
 			policies.add(new PerunPolicy(policyName, perunRoles, includePolicies));
 		}
@@ -96,14 +112,14 @@ public class PerunRolesLoader {
 
 	public void loadActionTypes(JdbcPerunTemplate jdbc) {
 		JsonNode rootNode = loadConfigurationFile();
-		JsonNode actionTypesNode = rootNode.get("perun_action_types");
+		JsonNode actionTypesNode = rootNode.get(PERUN_ACTION_TYPES);
 		List<JsonNode> allActionTypes = new ArrayList<>(new ObjectMapper().convertValue(actionTypesNode, new TypeReference<List<JsonNode>>() {
 		}));
 
 		for (JsonNode actionTypeNode : allActionTypes) {
-			String actionTypeName = actionTypeNode.get("name").isNull() ? null : actionTypeNode.get("name").textValue().toLowerCase();
-			String actionTypeObject = actionTypeNode.get("object").isNull() ? null : actionTypeNode.get("object").textValue();
-			String actionTypeDescription = actionTypeNode.get("description").isNull() ? null : actionTypeNode.get("description").textValue();
+			String actionTypeName = actionTypeNode.get(NAME).isNull() ? null : actionTypeNode.get(NAME).textValue().toLowerCase();
+			String actionTypeObject = actionTypeNode.get(OBJECT).isNull() ? null : actionTypeNode.get(OBJECT).textValue();
+			String actionTypeDescription = actionTypeNode.get(DESCRIPTION).isNull() ? null : actionTypeNode.get(DESCRIPTION).textValue();
 			boolean isMissing = false;
 			try {
 				if (actionTypeObject == null)
@@ -115,7 +131,7 @@ public class PerunRolesLoader {
 					if (BeansUtils.isPerunReadOnly()) {
 						throw new InternalErrorException("One of default actionType not exists in DB - " + actionTypeName + ": " + actionTypeObject);
 					} else {
-						int newId = Utils.getNewId(jdbc, "action_types_seq");
+						int newId = Utils.getNewId(jdbc, ACTION_TYPES_SEQ);
 						jdbc.update("insert into action_types (id, action_type, description, object) values (?,?,?,?)", newId, actionTypeName, actionTypeDescription, actionTypeObject);
 					}
 				}
@@ -129,7 +145,7 @@ public class PerunRolesLoader {
 		Map<String, RoleManagementRules> rolesManagementRules = new HashMap<>();
 		JsonNode rootNode = loadConfigurationFile();
 		//Fetch all policies from the configuration file
-		JsonNode rolesNodes = rootNode.get("perun_roles_management");
+		JsonNode rolesNodes = rootNode.get(PERUN_ROLES_MANAGEMENT);
 
 		// For each role node construct RoleManagementRules and add it to the map
 		Iterator<String> roleNames =rolesNodes.fieldNames();
@@ -137,7 +153,7 @@ public class PerunRolesLoader {
 			String roleName = roleNames.next();
 			JsonNode roleNode = rolesNodes.get(roleName);
 			List<Map<String, String>> privilegedRoles = new ArrayList<>();
-			JsonNode privilegedRolesNode = roleNode.get("privileged_roles");
+			JsonNode privilegedRolesNode = roleNode.get(PRIVILEGED_ROLES);
 
 			//Field privileged_roles is saved as List of maps in the for loop
 			for (JsonNode privilegedRoleNode : privilegedRolesNode) {
@@ -145,8 +161,8 @@ public class PerunRolesLoader {
 				privilegedRoles.add(innerRoleMap);
 			}
 
-			Map<String, String> entitiesToManage = createmapFromJsonNode(roleNode.get("entities_to_manage"));
-			Map<String, String> objectsToAssign = createmapFromJsonNode(roleNode.get("assign_to_objects"));
+			Map<String, String> entitiesToManage = createmapFromJsonNode(roleNode.get(ENTITIES_TO_MANAGE));
+			Map<String, String> objectsToAssign = createmapFromJsonNode(roleNode.get(ASSIGN_TO_OBJECTS));
 
 			rolesManagementRules.put(roleName, new RoleManagementRules(roleName, privilegedRoles, entitiesToManage, objectsToAssign));
 		}
